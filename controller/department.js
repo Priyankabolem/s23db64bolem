dc04741d868340fe4d9403579d61231321bad0d2
Priyankabolem <bolempriyanka10@gmail.com>
2023-04-11 13:25:21 -0500

    dept routes updated

--- controller/department.js
+++ controller/department.js
@@ -1,4 +1,17 @@
 var Department = require('../models/department');
+// VIEWS
+// Handle a show all view
+exports.department_view_all_Page = async function(req, res) {
+    try{
+    var theDepartment = await Department.find();
+    res.render('department', { title: 'Department Search Results', results: theDepartment });
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
+
 // List of all Department
 exports.department_list = async function(req, res) {
     try{
--- routes/department.js
+++ routes/department.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('department', { title: 'Search Results Department' });
-});
+const department_controlers= require('../controller/department');
+
+
+router.get('/',department_controlers.department_view_all_Page );
 
 module.exports = router;
