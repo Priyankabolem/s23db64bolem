e681e97e07a0ca4c94fa6c989dc0c3a36a18d3cd
Priyankabolem <123031760+Priyankabolem@users.noreply.github.com>
2023-04-20 00:00:06 +0530

    Create View Page and controller added

--- controller/department.js
+++ controller/department.js
@@ -99,4 +99,19 @@ exports.department_view_one_Page = async function(req, res) {
     res.send(`{'error': '${err}'}`);
     }
 }
+
+// Handle building the view for creating a department.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.department_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('departmentcreate', { title: 'Department Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
     
--- routes/department.js
+++ routes/department.js
@@ -1,11 +1,12 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-const department_controllers= require('../controller/department');
+const department_controllers = require("../controller/department");
 
+router.get("/", department_controllers.department_view_all_Page);
 
-router.get('/',department_controllers.department_view_all_Page );
-
-router.get('/details',department_controllers.department_view_one_Page);
+router.get("/details", department_controllers.department_view_one_Page);
+// GET create department page */
+router.get("/create", department_controllers.department_create_Page);
 module.exports = router;
