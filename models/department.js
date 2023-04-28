cc35fcb3693882428fd4fae08aaf9ae51a515fd7
Priyankabolem <123031760+Priyankabolem@users.noreply.github.com>
2023-04-28 08:21:08 +0530

    validations added to department model

--- models/department.js
+++ models/department.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
-const departmentSchema = mongoose.Schema({
-  Dept_Name: String,
-  Faculty_Strength: Number,
-  Total_Intake: Number,
-},{ collection:'Department',timestamps:true});
+const departmentSchema = mongoose.Schema(
+  {
+    Dept_Name: String,
+    Faculty_Strength:{type: Number,min:20,max:150},
+    Total_Intake: {type: Number,min:20,max:3500},
+  },
+  { collection: "Department", timestamps: true }
+);
 module.exports = mongoose.model("Department", departmentSchema);
